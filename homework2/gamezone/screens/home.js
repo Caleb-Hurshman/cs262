0a63f8a0c8e886f98f8f50b4db22410bb2e26a6a
Caleb Hurshman <caleb@hurshman.com>
2020-11-07 16:08:11 -0500

    initial commit

--- homework2/gamezone/screens/home.js
+++ homework2/gamezone/screens/home.js
@@ -5,7 +5,7 @@ import Card from '../shared/card';
 
 export default function Home({ navigation }) {
     const [isLoading, setLoading] = useState(true);
-    const [data, setdata] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         fetch('https://cs262-monopoly-service.herokuapp.com/players/')
@@ -18,9 +18,9 @@ export default function Home({ navigation }) {
     return (
         <View style={globalStyles.container}>
             <FlatList data={data} renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => navigation.navigate('PlayerDetails', item)}>
+                <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
                     <Card>
-                        <Text style={globalStyles.titleText}>{ item.id }, {item.email}, {item.name}</Text>
+                        <Text style={globalStyles.titleText}>{item.email}</Text>
                     </Card>
                 </TouchableOpacity>
             )} />
